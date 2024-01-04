376f96ecd9aa4f924ad66a202b5932908bf91ba9
pugachev.nv <pugachev.nv@gmail.com>
2024-01-04 23:32:07 +0300

    first commit AsyncForm

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -13,7 +13,11 @@ class AsyncForm {
    * через registerEvents()
    * */
   constructor(element) {
-
+    this.element = element;
+    this.registerEvents();
+    if (!element) {
+      throw new Error('Переданный элемент не существует!');
+    }
   }
 
   /**
@@ -21,7 +25,10 @@ class AsyncForm {
    * вызывает метод submit()
    * */
   registerEvents() {
-
+    this.element.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.submit();
+    })
   }
 
   /**
@@ -32,7 +39,10 @@ class AsyncForm {
    * }
    * */
   getData() {
+    const form = document.getElementById( 'login-form' ),
+      formData = new FormData( form );
 
+    return Object.fromEntries(formData.entries());
   }
 
   onSubmit(options){
@@ -44,6 +54,6 @@ class AsyncForm {
    * данные, полученные из метода getData()
    * */
   submit() {
-
+    this.onSubmit(this.getData())
   }
 }
